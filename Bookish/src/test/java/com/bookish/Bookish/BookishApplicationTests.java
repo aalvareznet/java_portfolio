e74966bea618f6ca15705d417af7c3005470bb63
aalvareznet <velurn123@gmail.com>
2024-12-28 18:01:12 -0600

    Adding the Bookish project
    This is a project that will recomend books based on the customer bookshelf

---
+++ Bookish/src/test/java/com/bookish/Bookish/BookishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookish.Bookish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
