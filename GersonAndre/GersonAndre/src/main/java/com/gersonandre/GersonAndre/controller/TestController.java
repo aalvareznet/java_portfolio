4ce783bda6effd8eb8b598bf0798810df7b848a5
Anthony Alvarez Delgado <107961797+TonyAlvaDB@users.noreply.github.com>
2025-01-05 21:40:13 -0600

    Added security.
    Will work on this tomorrow.

---
+++ GersonAndre/GersonAndre/src/main/java/com/gersonandre/GersonAndre/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.gersonandre.GersonAndre.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+    @PostMapping(value = "demo")
+    public String test(){
+        return "test";
+    }
+}
