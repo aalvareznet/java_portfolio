248cc91aab2eef68bfdc62cc2d243292b81086c4
Anthony Alvarez Delgado <107961797+TonyAlvaDB@users.noreply.github.com>
2025-01-22 17:26:25 -0600

    mappers working

--- GersonAndre/GersonAndre/src/main/java/com/gersonandre/GersonAndre/mapper/AccessoryMapper.java
+++ GersonAndre/GersonAndre/src/main/java/com/gersonandre/GersonAndre/mapper/AccessoryMapper.java
@@ -3,6 +3,7 @@ package com.gersonandre.GersonAndre.mapper;
 import com.gersonandre.GersonAndre.dto.AccessoryDto;
 import com.gersonandre.GersonAndre.model.Accessory;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
