54140ac62ee31910d1f07659e66d9017073a3bd2
Anthony Alvarez Delgado <107961797+TonyAlvaDB@users.noreply.github.com>
2025-03-10 22:00:22 -0600

    Initialize msvc-profile module; add application properties, banner, .gitignore, and Maven wrapper

---
+++ gestion-gimnasios/msvc-profile/src/main/java/com/anthony/msvc/profile/msvc_profile/MsvcProfileApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.msvc.profile.msvc_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcProfileApplication.class, args);
+	}
+
+}
---
+++ gestion-gimnasios/msvc-profile/src/test/java/com/anthony/msvc/profile/msvc_profile/MsvcProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.msvc.profile.msvc_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
