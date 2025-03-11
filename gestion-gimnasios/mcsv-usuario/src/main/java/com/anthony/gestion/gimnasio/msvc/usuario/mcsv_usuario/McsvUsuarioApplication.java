927b465224c6e0a6c646e0c44d1c879d06531680
Anthony Alvarez Delgado <107961797+TonyAlvaDB@users.noreply.github.com>
2025-03-10 18:07:12 -0600

    Creating proyect

---
+++ gestion-gimnasios/mcsv-usuario/src/main/java/com/anthony/gestion/gimnasio/msvc/usuario/mcsv_usuario/McsvUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.gestion.gimnasio.msvc.usuario.mcsv_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvUsuarioApplication.class, args);
+	}
+
+}
---
+++ gestion-gimnasios/mcsv-usuario/src/test/java/com/anthony/gestion/gimnasio/msvc/usuario/mcsv_usuario/McsvUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.gestion.gimnasio.msvc.usuario.mcsv_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
