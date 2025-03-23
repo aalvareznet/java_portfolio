13b6f38e3cc8acb45f7276c08d7e53628e840907
Anthony Alvarez Delgado <107961797+TonyAlvaDB@users.noreply.github.com>
2025-03-23 10:23:24 -0600

    Refactor Proveedor control and service to integrate DTOs, improve method structure, and add auditing functionality

--- ing-software-dunamys-v3/ing-software-dunamys-v3/src/main/java/com/uia/ing/soft/olda/dunamys/ing_software_dunamys/Controller/ProveedorControlador.java
+++ ing-software-dunamys-v3/ing-software-dunamys-v3/src/main/java/com/uia/ing/soft/olda/dunamys/ing_software_dunamys/Controller/ProveedorControlador.java
@@ -1,7 +1,6 @@
 package com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Model.Proveedor;
-import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Service.LogAuditoriaServicio;
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Dto.ProveedorCrearDto;
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Dto.ProveedorDto;
 import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Service.ProveedorServicio;
 
 @RestController
@@ -23,47 +22,39 @@ import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Service.ProveedorServi
 public class ProveedorControlador {
     @Autowired
     private ProveedorServicio servicio;
-    @Autowired
-    private LogAuditoriaServicio auditoria;
     
     @PostMapping("/{userId}")
-    public ResponseEntity<Proveedor> agregarProveedor(@PathVariable Integer userId
-                                                    , @RequestBody Proveedor proveedor){
-        Proveedor proveedorCreado = servicio.create(proveedor);
-        if (proveedorCreado != null) {
-            auditoria.guardarAccion(userId, "Proveedor creado", "proveedor");
+    public ResponseEntity<ProveedorDto> agregarProveedor(@PathVariable Integer userId
+                                                    , @RequestBody ProveedorCrearDto proveedor){
+        ProveedorDto proveedorCreado = servicio.agregar(userId, proveedor);
+        if(proveedorCreado != null){
             return ResponseEntity.ok(proveedorCreado);
         }
         return ResponseEntity.badRequest().build();
+
     }
     @GetMapping
-    public ResponseEntity<List<Proveedor>> obtenerTodosLosProveedores(){
-        List<Proveedor> busquedaProveedor = servicio.findAll();
-        if(!busquedaProveedor.isEmpty()){
-            return ResponseEntity.ok(busquedaProveedor);
+    public ResponseEntity<List<ProveedorDto>> obtenerTodosLosProveedores(){
+        List<ProveedorDto> proveedores = servicio.obtenerProveedores();
+        if(proveedores.size() > 0){
+            return ResponseEntity.ok(proveedores);
         }
         return ResponseEntity.noContent().build();
     }
     @GetMapping("/{id}")
-    public ResponseEntity<Proveedor> obtenerProveedorPorId(@PathVariable Integer id){
-        Optional<Proveedor> busquedaProveedor = servicio.findById(id);
-        if(busquedaProveedor.isPresent()){
-            return ResponseEntity.ok(busquedaProveedor.get());
+    public ResponseEntity<ProveedorDto> obtenerProveedorPorId(@PathVariable Integer id){
+        ProveedorDto proveedor = servicio.obtenerProveedor(id);
+        if(proveedor != null){
+            return ResponseEntity.ok(proveedor);
         }
         return ResponseEntity.notFound().build();
     }
     @PutMapping("/{id}/actualizar/{userId}")
-    public ResponseEntity<Proveedor> actualizarProveedor(@PathVariable Integer id
-                                                        , @RequestBody Proveedor proveedor
+    public ResponseEntity<ProveedorDto> actualizarProveedor(@PathVariable Integer id
+                                                        , @RequestBody ProveedorCrearDto proveedor
                                                         , @PathVariable Integer userId){
-        Optional<Proveedor> busquedaProveedor = servicio.findById(id);
-        if(busquedaProveedor.isPresent()){
-            Proveedor proveedorParaActualizar = busquedaProveedor.get();
-            proveedorParaActualizar.setNombre(proveedor.getNombre());
-            proveedorParaActualizar.setTelefono(proveedor.getTelefono());
-            proveedorParaActualizar.setCorreoReferencia(proveedor.getCorreoReferencia());
-            Proveedor proveedorActualizado = servicio.update(proveedorParaActualizar);
-            auditoria.guardarAccion(userId, "Proveedor con el ID " + proveedorActualizado.getId() + " actualizado", "proveedor");
+        ProveedorDto proveedorActualizado = servicio.actualizar(id, proveedor, userId);
+        if(proveedorActualizado != null){
             return ResponseEntity.ok(proveedorActualizado);
         }
         return ResponseEntity.notFound().build();
@@ -71,11 +62,9 @@ public class ProveedorControlador {
     @DeleteMapping("/{id}/{userId}")
     public ResponseEntity<String> borrarProveedor(@PathVariable Integer id
                                                 , @PathVariable Integer userId){
-        Optional<Proveedor> busquedaProveedor = servicio.findById(id);
-        if(busquedaProveedor.isPresent()){
-            servicio.delete(id);
-            auditoria.guardarAccion(userId, "Proveedor ID " + busquedaProveedor.get().getId() + " eliminado", "proveedor");
-            return ResponseEntity.ok("Proveedor ID " + busquedaProveedor.get().getId() + " eliminado");
+        String mensaje = servicio.borrar(id, userId);
+        if(mensaje != null){
+            return ResponseEntity.ok(mensaje);
         }
         return ResponseEntity.notFound().build();
     }
--- ing-software-dunamys-v3/ing-software-dunamys-v3/src/main/java/com/uia/ing/soft/olda/dunamys/ing_software_dunamys/Service/ProveedorServicio.java
+++ ing-software-dunamys-v3/ing-software-dunamys-v3/src/main/java/com/uia/ing/soft/olda/dunamys/ing_software_dunamys/Service/ProveedorServicio.java
@@ -1,8 +1,14 @@
 package com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Dto.ProveedorCrearDto;
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Dto.ProveedorDto;
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Mappers.ProveedorMapper;
 import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Model.Proveedor;
 import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Repository.ProveedorRepositorio;
 
@@ -12,9 +18,59 @@ public class ProveedorServicio extends BaseService<Proveedor, Integer>{
 
     @Autowired
     private ProveedorRepositorio repo;
+    @Autowired
+    private LogAuditoriaServicio auditoria;
+    @Autowired
+    private ProveedorMapper mapper;
 
     protected ProveedorRepositorio getRepository() {
         return repo;
     }
 
+    public ProveedorDto agregar(Integer usuarioId, ProveedorCrearDto proveedorDto){
+        Proveedor proveedor = mapper.convertToEntity(proveedorDto);
+        Proveedor proveedorCreado = create(proveedor);
+        if (proveedorCreado != null) {
+            auditoria.guardarAccion(usuarioId, "Proveedor creado", "proveedor");
+            return mapper.convertToDto(proveedorCreado);
+        }
+        return null;
+    }
+
+    public List<ProveedorDto> obtenerProveedores(){
+        List<Proveedor> proveedores = this.findAll();
+        List<ProveedorDto> proveedoresDto = proveedores.stream().map(proveedor -> mapper.convertToDto(proveedor)).toList();
+        return proveedoresDto;
+    }
+
+    public ProveedorDto obtenerProveedor(Integer id){
+        Optional<Proveedor> proveedor = this.findById(id);
+        if(proveedor.isPresent()){
+            return mapper.convertToDto(proveedor.get());
+        }
+        return null;
+    }
+    public ProveedorDto actualizar(Integer id, ProveedorCrearDto proveedorDto, Integer usuarioId){
+        Optional<Proveedor> proveedorEncontrado = this.findById(id);
+        if(proveedorEncontrado.isPresent()){
+            Proveedor proveedorEntrante = mapper.convertToEntity(proveedorDto);
+            Proveedor proveedorParaActualizar = proveedorEncontrado.get();
+            proveedorParaActualizar.setNombre(proveedorEntrante.getNombre());
+            proveedorParaActualizar.setCorreoReferencia(proveedorEntrante.getCorreoReferencia());
+            proveedorParaActualizar.setTelefono(proveedorEntrante.getTelefono());            
+            Proveedor proveedorActualizado = this.update(proveedorParaActualizar);
+            auditoria.guardarAccion(usuarioId, "Proveedor actualizado", "proveedor");
+            return mapper.convertToDto(proveedorActualizado);
+        }
+        return null;
+    }
+    public String borrar(Integer usuarioId, Integer id){
+        Optional<Proveedor> proveedor = this.findById(id);
+        if(proveedor.isPresent()){
+            this.delete(id);
+            auditoria.guardarAccion(usuarioId, "Proveedor eliminado", "proveedor");
+            return "Proveedor eliminado";
+        }
+        return null;
+    }
 }
