4771a6a92e0905d84578e131ed7cb8fd355a375e
Anthony Alvarez Delgado <107961797+TonyAlvaDB@users.noreply.github.com>
2025-03-25 16:31:27 -0600

    Add validation annotations to CategoriaInventarioCrearDto and ClienteCrearDto for improved data integrity

--- ing-software-dunamys-v3/ing-software-dunamys-v3/src/main/java/com/uia/ing/soft/olda/dunamys/ing_software_dunamys/Dto/CategoriaInventarioCrearDto.java
+++ ing-software-dunamys-v3/ing-software-dunamys-v3/src/main/java/com/uia/ing/soft/olda/dunamys/ing_software_dunamys/Dto/CategoriaInventarioCrearDto.java
@@ -1,8 +1,17 @@
 package com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Dto;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
+@Valid
 public class CategoriaInventarioCrearDto {
+    @NotBlank(message = "La categoria no puede estar en blanco")
+    @NotEmpty(message = "La categoria no puede estar vacia")
+    @Size(min = 3, max = 20, message = "La categoria debe tener un minimo de 3 caracteres y un maximo de 20")
     String categoria;
 }
