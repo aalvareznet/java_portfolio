a55ec9b99d978eb88b9afaa710b03f9e1aad5f55
Anthony Alvarez Delgado <107961797+TonyAlvaDB@users.noreply.github.com>
2025-04-05 19:45:08 -0600

    Enhance JWT token generation to include client details and update client report template to display additional fields

--- ing-software-dunamys-v3/ing-software-dunamys-v3/src/main/java/com/uia/ing/soft/olda/dunamys/ing_software_dunamys/Security/jwt/JwtService.java
+++ ing-software-dunamys-v3/ing-software-dunamys-v3/src/main/java/com/uia/ing/soft/olda/dunamys/ing_software_dunamys/Security/jwt/JwtService.java
@@ -1,10 +1,17 @@
 package com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Security.jwt;
 
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Dto.ClienteInfoPersonaDto;
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Mappers.ClienteMapper;
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Model.Cliente;
+import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Repository.ClienteRepositorio;
 import com.uia.ing.soft.olda.dunamys.ing_software_dunamys.Security.user.User;
+    
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -12,6 +19,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -19,16 +27,26 @@ public class JwtService {
 
     @Value("${jwt.secret}")
     private String SECRET_KEY;
+    @Autowired
+    private ClienteRepositorio clienteRepositorio;
+    @Autowired
+    private ClienteMapper clienteMapper;
 
     public String getToken(User user) {
         return getToken(new HashMap<>(), user);
     }
 
     private String getToken(HashMap<String,Object> extraClaims, User user) {
+        Optional<Cliente> clienteOptional = clienteRepositorio.findById(user.getId());
+        ClienteInfoPersonaDto cliente = clienteMapper.ConvertEntityToInfoDto(clienteOptional.get());
         return Jwts.builder()
                 .claims(extraClaims)
                 .claim("userId", user.getId())
                 .claim("autorities", "ROLE_"+user.getRole().name())
+                .claim("nombre", cliente.getNombrePersona())
+                .claim("apellido", cliente.getPrimerApellidoPersona() + " " + cliente.getSegundoApellidoPerson())
+                .claim("correo", cliente.getEmail())
+                .claim("telefono", cliente.getTelefono())
                 .subject(user.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis()+1000*60*24))
